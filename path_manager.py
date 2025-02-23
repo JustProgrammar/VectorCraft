64c646cb8c0f7486e85d54655e2282d78ef1c150
6newyayworld1 <39933347-6newyayworld1@users.noreply.replit.com>
2025-02-23 16:22:35 +0000

    Agent query: Can you test the SVG export feature? Try drawing some paths and then click the 'Export SVG' button to see if it shows the SVG commands correctly.
    Add SVG export functionality:  Allows users to export vector glyphs as SVG path commands, displaying them in a dialog.

--- path_manager.py
+++ path_manager.py
@@ -15,6 +15,7 @@ class Path:
         self.is_closed = False
         self.stroke_width = 2
         self.stroke_color = "#000000"
+        self.fill = "none"
 
     def add_point(self, position, handle_in=None, handle_out=None):
         point = PathPoint(
@@ -33,30 +34,43 @@ class Path:
         for i in range(len(self.points)):
             current = self.points[i]
             bezier_points.append(current.position)
-            
+
             if current.handle_out is not None:
                 bezier_points.append(current.handle_out)
-            
+
             next_point = self.points[(i + 1) % len(self.points)]
             if next_point.handle_in is not None:
                 bezier_points.append(next_point.handle_in)
-                
+
             if i == len(self.points) - 1 and not self.is_closed:
                 break
-                
+
         return bezier_points
 
     def to_svg(self):
-        return generate_svg_path(self.get_bezier_points())
+        path_data = generate_svg_path(self.get_bezier_points())
+        return f'<path d="{path_data}" stroke="{self.stroke_color}" stroke-width="{self.stroke_width}" fill="{self.fill}"/>'
 
 class PathManager:
     def __init__(self):
         self.paths = []
         self.current_path = None
-        
+
     def start_new_path(self):
         self.current_path = Path()
         self.paths.append(self.current_path)
-        
+
     def get_current_path(self):
         return self.current_path
+
+    def export_svg(self):
+        svg_paths = []
+        for path in self.paths:
+            svg_paths.append(path.to_svg())
+
+        svg_content = (
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 600">\n'
+            f'  {"".join(svg_paths)}\n'
+            '</svg>'
+        )
+        return svg_content
